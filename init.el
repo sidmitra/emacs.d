6cc4d3bc9788d48d52ce2ac2e31005b0ff7df12e
Sid Mitra <sidmitra.del@gmail.com>
2015-03-17 17:51:56 -0300

    Makes helm window leaner.

--- init.el
+++ init.el
@@ -162,6 +162,15 @@
 
 (helm-mode 1)
 
+;; https://www.reddit.com/r/emacs/comments/2z7nbv/lean_helm_window/
+(setq helm-display-header-line nil) ;; t by default
+(set-face-attribute 'helm-source-header nil :height 0.1)
+(helm-autoresize-mode 1)
+(defun helm-toggle-header-line ()
+  (if (= (length helm-sources) 1)
+      (set-face-attribute 'helm-source-header nil :height 0.1)
+    (set-face-attribute 'helm-source-header nil :height 1.0)))
+(add-hook 'helm-before-initialize-hook 'helm-toggle-header-line)
 
 ;; html
 ;; ====
