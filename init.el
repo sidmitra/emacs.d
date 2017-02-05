00c0afea11504ada6d8aba083ff0721aec3e20ae
Sid Mitra <sidmitra.del@gmail.com>
2017-02-04 23:25:41 -0300

    Updates web-mode config

--- init.el
+++ init.el
@@ -602,12 +602,17 @@
          ("\\.scss$" . web-mode)
          ("\\.less$" . web-mode))
   :config
+  (setq-default indent-tabs-mode nil) ;; no TABS
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
-  (setq web-mode-enable-current-element-highlight t)
+  ;; (setq web-mode-commet-style 2)
   (setq web-mode-enable-auto-pairing t)
-  (setq web-mode-enable-current-column-highlight t))
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t)
+  (setq web-mode-enable-current-column-highlight t)
+  (setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-enable-css-colorization t))
 ;;  (setq js-indent-level 2)
 
 (use-package company-web
