3996fd49f0159ee6faa062e4e214e8cd1c907b70
Sid Mitra <sidmitra.del@gmail.com>
2017-02-04 23:28:50 -0300

    Adds ein config. Will enable after testing

--- init.el
+++ init.el
@@ -455,6 +455,15 @@
 ;;   :ensure t
 ;;   :config)
 
+;; (use-package ein
+;;   :ensure t
+;;   :config
+;;   (setq ein:use-auto-complete t)
+;;   ;; Or, to enable "superpack" (a little bit hacky improvements):
+;;   ;; (setq ein:use-auto-complete-superpack t)
+;;   (setq ein:use-smartrep t))
+
+
 
 ;; Layout
 ;; ========
