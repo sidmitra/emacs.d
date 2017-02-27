fda4bde3ea33beec326ca7ae856adf4cd74d27aa
Sid Mitra <sidmitra.del@gmail.com>
2017-02-27 14:39:48 -0300

    Replaces existing modeline config with spaceline.

--- init.el
+++ init.el
@@ -367,8 +367,26 @@
 (use-package powerline
   :ensure t
   :config
-  (powerline-center-theme)
-  (setq-default powerline-default-separator 'curve))
+  ;;(powerline-center-theme)
+  ;;(setq-default powerline-default-separator 'curve)
+  )
+(use-package spaceline
+  :ensure t
+  :config
+  (progn
+    (require 'spaceline-config)
+    (setq powerline-default-separator 'slant)
+    (setq spaceline-workspace-numbers-unicode t)
+    (setq spaceline-separator-dir-left '(left . left))
+    (setq spaceline-separator-dir-right '(right . right))
+    (setq powerline-height 27)
+    (spaceline-toggle-window-number-on)
+    (spaceline-toggle-buffer-modified-on)
+    (spaceline-toggle-major-mode-on)
+    (spaceline-toggle-battery-on)
+    (spaceline-toggle-hud-on)
+    (spaceline-toggle-projectile-root-on)
+    (spaceline-emacs-theme)))
 
 
 ;; mode-icons
