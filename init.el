b790fa551bc5238c4b82197fa486c49cd2c4a5a6
Sid Mitra <sidmitra.del@gmail.com>
2017-04-21 17:33:15 -0300

    Refactoring configuration to use new outline structure.

--- init.el
+++ init.el
@@ -12,3 +12,19 @@
 
 ;; Load my custom configuration
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
+;; Automatically added
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet yaml-mode emmet-mode vue-mode eslint-fix company-web web-mode undo-tree tabbar rainbow-delimiters rainbow-mode py-isort company-anaconda anaconda-mode helm-projectile projectile neotree multi-term mode-icons spaceline powerline flymd markdown-mode magit go-mode flycheck exec-path-from-shell dockerfile-mode company beacon helm-ag ag origami multiple-cursors move-text cursor-in-brackets smooth-scrolling dracula-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
