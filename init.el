6e026de48a55f5e8232e76bf05938cb741a52f13
Sid Mitra <sidmitra.del@gmail.com>
2018-08-07 14:40:13 -0400

    Updates ag-arguments. Enables narrow-to-region by default

--- init.el
+++ init.el
@@ -19,6 +19,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-arguments
+   (quote
+    ("--ignore-dir" "node_modules" "--ignore-dir" "elpa" "--ignore-dir" "lib" "--ignore-dir" "build")))
  '(custom-safe-themes
    (quote
     ("e2aa315770b0041d2b1034a6913e5df2029fc869347b407d5f3d1311e347ef7b" default)))
@@ -31,3 +34,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'narrow-to-region 'disabled nil)
