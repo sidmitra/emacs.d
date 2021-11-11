a1bc5ae9772d81e9f441de55bcde3624fb92a1cf
Sid Mitra <sidmitra.del@gmail.com>
2021-11-11 12:11:37 -0300

    Trying fix for org-mode load issues
    - Changed theme to nano
    - Commented out org-super-agenda temporarily
    - update elfeed feeds
    - Added trial configs for box, EAF

--- init.el
+++ init.el
@@ -38,6 +38,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1ca05bdae217adeb636e9bc5e84c8f1d045be2c8004fafd5337d141d9b67a96f" "aca70b555c57572be1b4e4cec57bc0445dcb24920b12fb1fea5f6baa7f2cad02" default))
  '(package-selected-packages
    '(eradio org-journal deft org-roam doct org-pretty-tags org-super-agenda org-superstar dockerfile-mode flymd flycheck-rust cargo rust-mode emmet-mode prettier-js company-web web-mode dotenv-mode toml-mode yaml-mode flycheck-posframe flycheck-prospector pyvenv lsp-mode diff-hl git-timemachine magit-delta browse-at-remote vc-msg forge magit format-all yasnippet-snippets yasnippet better-shell vterm keyfreq undo-tree company-statistics ag ctrlf company-prescient selectrum-prescient prescient selectrum neotree dumb-jump projectile bm which-key-posframe which-key comment-dwim-2 expand-region move-text whole-line-or-region multiple-cursors editorconfig browse-kill-ring mode-icons minions doom-modeline nlinum-hl nlinum eyebrowse rainbow-mode rainbow-delimiters highlight-indent-guides color-identifiers-mode unicode-fonts all-the-icons doom-themes exec-path-from-shell paradox use-package))
  '(paradox-github-token t))
