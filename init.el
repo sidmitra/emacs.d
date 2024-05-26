1c0f11f95c957aa8cdea209fd2629be83a22643b
Sid Mitra <sidmitra.del@gmail.com>
2024-05-26 19:39:26 -0400

    Switch theme to doom-one-light, uncomment eglot
    - Update magit config
    - Add poetry

--- init.el
+++ init.el
@@ -95,3 +95,16 @@
 
 
 (put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
