f481091aa4bac718c61447408c0566982f9461ce
Sid Mitra <sidmitra.del@gmail.com>
2024-08-31 21:56:12 -0400

    Update autocomplete in buffer config
    - Add helper to find-file with line number

--- init.el
+++ init.el
@@ -102,7 +102,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e"
-     "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default)))
+     "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
+ '(package-selected-packages '(devdocs)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
