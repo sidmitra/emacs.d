51983f4c3a1ba153a4cd60c1df6885576aa3ce7f
Sid Mitra <github@sidmitra.com>
2026-03-03 23:07:07 +0530

    Update default gptel model to gemini
    - Add claude-code, agent-shell. Keeping it commented for now to experiment

--- init.el
+++ init.el
@@ -105,7 +105,10 @@
      "48cfb7c9e5ad83901f9789790a7ae427ab4242005c2ee6d0b0f0b5ff5e3f724e"
      "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e"
      "9f297216c88ca3f47e5f10f8bd884ab24ac5bc9d884f0f23589b0a46a608fe14" default))
- '(package-selected-packages '(devdocs)))
+ '(package-selected-packages '(claude-code devdocs monet))
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el")
+     (monet :url "https://github.com/stevemolitor/monet"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
